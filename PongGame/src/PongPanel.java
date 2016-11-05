381dbdcb8881760f47430b76e2fb6a1de01a20e8
KhoaPham <phamdangkhoa0403@gmail.com>
2016-11-05 15:15:15 +0700

    Lỗi: Chiều dài thanh chắn của Player 2 dài hơn chiều dài thanh chắn của Player 1.

--- PongGame/src/PongPanel.java
+++ PongGame/src/PongPanel.java
@@ -61,10 +61,10 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	private int playerOneHeight = 50;
 
 	/** Player 2's paddle: position and size */
-	private int playerTwoX = 465;
+	private int playerTwoX = 480;
 	private int playerTwoY = 250;
 	private int playerTwoWidth = 10;
-	private int playerTwoHeight = 60;
+	private int playerTwoHeight = 50;
 
 	/** Speed of the paddle - How fast the paddle move. */
 	private int paddleSpeed = 5;
